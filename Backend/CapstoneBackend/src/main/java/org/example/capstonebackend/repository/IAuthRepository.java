298422c1014fa849bcdcc0c62ebff5dafb2bfc6f
ekaiser <ekaiser@indeed.com>
2024-05-19 18:21:18 -0500

    completed IAuthRepository

--- Backend/CapstoneBackend/src/main/java/org/example/capstonebackend/repository/IAuthRepository.java
+++ Backend/CapstoneBackend/src/main/java/org/example/capstonebackend/repository/IAuthRepository.java
@@ -1,4 +1,9 @@
 package org.example.capstonebackend.repository;
 
-public interface IAuthRepository {
+import org.example.capstonebackend.model.Auth;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IAuthRepository extends JpaRepository<Auth, Integer> {
 }
