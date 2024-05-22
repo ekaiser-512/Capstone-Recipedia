2ac808d86f16a5862bf5f8f67eafc7183d1cf024
ekaiser <ekaiser@indeed.com>
2024-05-22 13:57:57 -0500

    created IngredientCRUD and TestUtility class

--- Backend/CapstoneBackend/src/main/java/org/example/capstonebackend/repository/IIngredientRepository.java
+++ Backend/CapstoneBackend/src/main/java/org/example/capstonebackend/repository/IIngredientRepository.java
@@ -4,6 +4,14 @@ import org.example.capstonebackend.model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IIngredientRepository extends JpaRepository<Ingredient, Integer> {
+    Optional<Ingredient> findByName(String ingredientName);
+
+    Optional<Ingredient> findByRestriction(String dietaryRestriction);
+
+    List<Ingredient> findByAllergen(Boolean commonAllergen);
 }
--- Backend/CapstoneBackend/src/test/java/org/example/capstonebackend/components/IngredientTestUtilities.java
+++ Backend/CapstoneBackend/src/test/java/org/example/capstonebackend/components/IngredientTestUtilities.java
@@ -1,4 +1,49 @@
 package org.example.capstonebackend.components;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.capstonebackend.model.Ingredient;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.core.Is.is;
+
+
+@Component
 public class IngredientTestUtilities {
+
+    static Ingredient ingredient = new Ingredient();
+
+    public static final Ingredient mockIngredient = createMockIngredient();
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static Ingredient createMockIngredient() {
+        Ingredient ingredient = new Ingredient();
+        ingredient.setIngredientId(1);
+        ingredient.setIngredientName("cheese");
+        ingredient.setIngredientFoodGroup("dairy");
+        ingredient.setDietaryRestriction("lactose");
+        ingredient.setCommonAllergen(true);
+
+        return ingredient;
+    }
+
+    public static String ingredientToJson(Ingredient mockIngredient) {
+        try {
+            return objectMapper.writeValueAsString(mockIngredient);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void compareJsonOutput(ResultActions resultActions, Ingredient mockIngredient) throws Exception {
+        resultActions
+                .andExpect(jsonPath("$.ingredientId", is(mockIngredient.getIngredientId())))
+                .andExpect(jsonPath("$.ingredientName", is(mockIngredient.getIngredientName())))
+                .andExpect(jsonPath("$.ingredientFoodGroup", is(mockIngredient.getIngredientFoodGroup())))
+                .andExpect(jsonPath("$.dietaryRestriction", is(mockIngredient.getDietaryRestriction())))
+                .andExpect(jsonPath("$.commonAllergen", is(mockIngredient.getCommonAllergen())));
+    }
+
 }
